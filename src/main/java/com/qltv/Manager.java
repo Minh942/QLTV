55fb9d3d5c49b84138f5f2a4c945135d74a0f2b3
Minh Phan <minh.phan@ntq-solution.com.vn>
2023-09-15 14:56:55 +0700

    create project

---
+++ src/main/java/com/qltv/Manager.java
@@ -0,0 +1,17 @@
+package com.qltv;
+
+import java.time.LocalDate;
+
+public class Manager {
+    public Sach ThemSach(Long id, String tenSach, String tenTacGia, LocalDate ngayThem, NhanVien nhanVien){
+        Sach sach = new Sach();
+
+
+
+        return sach;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/qltv/NhanVien.java
@@ -0,0 +1,22 @@
+package com.qltv;
+
+public class NhanVien {
+    private Long id;
+    private String hoTen;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+}
